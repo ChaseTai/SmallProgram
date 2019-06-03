1815678c2fd5367e7183aac30115862a19e90136
ChaseTai <605861422@qq.com>
2019-06-04 00:21:40 +0800

    修改文件

--- Demo/pages/posts/post-detail/post-detail.js
+++ Demo/pages/posts/post-detail/post-detail.js
@@ -30,14 +30,32 @@ Page({
     }
   },
   onCollectionTap: function(event){
+    this.getPostCollectedSyc();
+    // this.getPostCollectedAsy();
+  },
+  getPostCollectedAsy:function(){
+    var that = this;
+    wx.getStorage({
+      key: 'posts_Collected',
+      success: function(res) {
+        var postsCollected = res.data;
+        var postCollected = postsCollected[that.data.currentPostId];
+        // 收藏变成未收藏，未收藏变成收藏
+        postCollected = !postCollected;
+        postsCollected[that.data.currentPostId] = postCollected;
+        that.showToast(postsCollected, postCollected);
+      },
+    })
+  },
+  getPostCollectedSyc: function(){
     var postsCollected = wx.getStorageSync('posts_Collected');
     var postCollected = postsCollected[this.data.currentPostId];
     // 收藏变成未收藏，未收藏变成收藏
     postCollected = !postCollected;
     postsCollected[this.data.currentPostId] = postCollected;
-    // this.showModal(postsCollected, postCollected);
     this.showToast(postsCollected, postCollected);
   },
+
   showModal: function (postsCollected, postCollected){
     var that = this;
     wx.showModal({
@@ -79,7 +97,7 @@ Page({
       "分享到朋友圈",
       "分享到QQ",
       "分享到微博"
-    ]
+    ];
     wx.showActionSheet({
       itemList: itemList,
       itemColor: "#405f80",
@@ -92,6 +110,14 @@ Page({
       fail: function(res){
         console.log(res.errMsg)
       }
+    })
+  },
+
+  onMusicTap:function(event){
+    wx.playBackgroundAudio({
+      dataUrl: 'http://ws.stream.qqmusic.qq.com/C100003507bR0gDKBm.m4a?fromtag=38',
+      title: '夜夜夜夜-齐秦',
+      coverImgUrl: 'http://y.gtimg.cn/music/photo_new/T002R150x150M000001TEc6V0kjpVC.jpg?max_age=2592000'
     })
   }
 })
