dc4baed722f80ea938a1f8f472f63544aa6525f0
cai.taihao <caitaihao@aldjf.com>
2019-06-05 17:29:50 +0800

    part-8(8-3)

---
+++ Demo/pages/movies/more-movie/more-movie.js
@@ -0,0 +1,16 @@
+// pages/movies/more-movie/more-movie.js
+Page({
+  data: {
+    navigateTitle: ''
+  },
+  onLoad: function (options) {
+    var category = options.category;
+    this.data.navigateTitle = category;
+  },
+
+  onReady: function (event) {
+    wx.setNavigationBarTitle({
+      title: this.data.navigateTitle
+    })
+  }
+})
